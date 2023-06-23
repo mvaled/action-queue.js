8e1d8a202e24c4168161e069e80b90befd14a89f
Manuel Vázquez Acosta <manuel@merchise.org>
2023-06-23 23:10:14 +0200

    Build UMD with the new name.

--- dist/action-queue.umd.js
+++ dist/action-queue.umd.js
@@ -1,5 +1,5 @@
 (function(global, factory) {
-  typeof exports === "object" && typeof module !== "undefined" ? factory(exports) : typeof define === "function" && define.amd ? define(["exports"], factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, factory(global["action-queue"] = {}));
+  typeof exports === "object" && typeof module !== "undefined" ? factory(exports) : typeof define === "function" && define.amd ? define(["exports"], factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, factory(global["@merchise/action-queue"] = {}));
 })(this, function(exports2) {
   "use strict";
   class ActionQueue {
